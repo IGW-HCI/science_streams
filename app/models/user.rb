4e7af82595aba540aab0d4b775994eaf5ea14d6f
Rene Koller <r.koller@me.com>
2016-07-25 10:06:28 +0200

    added user profile model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_one :profile
   has_many :project_users
   has_many :projects, through: :project_users
 end
